3d6db633062b3399b80e4fe18623505f95cb264b
Juliana-1234 <jhulyanaaggrey@gmail.com>
2022-07-04 09:22:29 -0700

    prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,13 +9,10 @@ int main(void)
 {
 	int ch;
 	for (ch = 97; ch <= 122; ch++)
-
 	{
 	
-		putchar(ch);
-	
+		putchar(ch);	
 	}
-	
 	putchar(10); /* this is an ascii code for new line*/
 
 	return (0);
