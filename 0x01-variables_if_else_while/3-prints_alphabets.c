dc74bffe80e90aca9f3bb6bbd4a4eb783f864ce7
Juliana-1234 <jhulyanaaggrey@gmail.com>
2022-07-04 22:25:38 -0700

    prints alphabets in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-prints_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable
+ *
+ * *Return : always 0
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <=90; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}
