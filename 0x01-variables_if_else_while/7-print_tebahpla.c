d9d35afc504a7e654dd171fa2462b45335ab6c85
Juliana-1234 <jhulyanaaggrey@gmail.com>
2022-07-05 06:22:35 -0700

    prints the lowercase alphabet in reversclear

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable
+ *
+ *Return : always 0
+ */
+int main(void)
+{
+	for (ch = 'z'; ch >= 'a'; ch++)
+	{
+		
+		putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}
