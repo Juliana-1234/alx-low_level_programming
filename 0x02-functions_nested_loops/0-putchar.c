fa53b737a06cc0409a29612941069ef6ca0cb074
Juliana-1234 <jhulyanaaggrey@gmail.com>
2022-07-07 05:29:21 -0700

    print _putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,34 @@
-| | (+ '#'include "main.h")
-| ^
-+ /**
-+  * _putchar - writes the character c to stdout
-+  * @c: The character to print
-+  *
-+  * Return: On success 1.
-+  * On error, -1 is returned, and errno is set appropriately.
-+  */
-+ int _putchar(char c)
-+ {
-+	return (write(1, &c, 1));
-+ }
+0-main.c code
+
+
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
+}
+
+
+
+main.h code
+
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif /* MAIN_H */
